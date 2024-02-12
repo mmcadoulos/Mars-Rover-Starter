d57499fdae61167cd816a1437f402ec2d4abc8bd
Μιχαίας <mmcadoulos@gmail.com>
2024-02-11 21:28:05 -0600

    finished task 3

--- message.js
+++ message.js
@@ -1,5 +1,12 @@
 class Message {
    // Write code here!
+   constructor(name, commands){
+      this.name = name;
+      if(!name){
+         throw Error("no name entered");
+      }
+      this.commands = commands;
+   }
 }
 
 module.exports = Message;
