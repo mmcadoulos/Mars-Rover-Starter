8b159d75a32f817b41786cb2fd250461f005eb68
Μιχαίας <mmcadoulos@gmail.com>
2024-02-17 17:14:00 -0600

    added sandbox, test 7, test 8

--- rover.js
+++ rover.js
@@ -1,5 +1,17 @@
 class Rover {
    // Write code here!
+   constructor(position){
+      this.position = position;
+      this.mode = "NORMAL";
+      this.generatorWatts = 110;
+   }
+   receiveMessage(message){
+      let response = {
+         "message": message.name,
+         //"results": 
+      }
+      return response;
+   }
 }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -1,13 +1,28 @@
-// const Rover = require('../rover.js');
-// const Message = require('../message.js');
-// const Command = require('../command.js');
+const Rover = require('../rover.js');
+const Message = require('../message.js');
+const Command = require('../command.js');
 
-// // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
-// //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
+// NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
+//       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 
-// describe("Rover class", function() {
+describe("Rover class", function() {
+    // 7 tests here!
+    it("constructor sets position and default values for mode and generatorWatts", ()=>{
+        let position = 0;
+        let testRover = new Rover(position);
+        expect(testRover.position).toEqual(0);
+        expect(testRover.mode).toBe("NORMAL");
+        expect(testRover.generatorWatts).toBe(110);
+    });
 
-//   // 7 tests here!
+    it("response returned by receiveMessage contains the name of the message", ()=>{
+        let testRover = new Rover(25);
+        let message = new Message("test");
+        let response = testRover.receiveMessage(message);
+        expect(response.message).toBe("test");
+    });
 
-// });
+
+
+});
